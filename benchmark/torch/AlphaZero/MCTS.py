7b664746cdcf35f947313fadcd04072d94be09f6
Adam Lawson <adamjlawson22@gmail.com>
2020-08-21 15:26:10 +0100

    Remove Dirichlet Noise

--- benchmark/torch/AlphaZero/MCTS.py
+++ benchmark/torch/AlphaZero/MCTS.py
@@ -16,11 +16,10 @@ class MCTS():
     This class handles the MCTS tree.
     """
 
-    def __init__(self, game, nn_agent, args, dirichlet_noise=False):
+    def __init__(self, game, nn_agent, args):
         self.game = game
         self.nn_agent = nn_agent
         self.args = args
-        self.dirichlet_noise = dirichlet_noise
         self.Qsa = {}  # stores Q values for s,a (as defined in the paper)
         self.Nsa = {}  # stores #times edge s,a was visited
         self.Ns = {}  # stores #times board s was visited
@@ -39,14 +38,10 @@ class MCTS():
                    proportional to Nsa[(s,a)]**(1./temp)
         """
         for i in range(self.args.numMCTSSims):
-            dir_noise = (i == 0 and self.dirichlet_noise)
-            self.search(canonicalBoard, dirichlet_noise=dir_noise)
+            self.search(canonicalBoard)
 
         s = self.game.stringRepresentation(canonicalBoard)
-        counts = [
-            self.Nsa[(s, a)] if (s, a) in self.Nsa else 0
-            for a in range(self.game.getActionSize())
-        ]
+        counts = [self.Nsa[(s, a)] if (s, a) in self.Nsa else 0 for a in range(self.game.getActionSize())]
 
         if temp == 0:
             bestAs = np.array(np.argwhere(counts == np.max(counts))).flatten()
@@ -60,7 +55,7 @@ class MCTS():
         probs = [x / counts_sum for x in counts]
         return probs
 
-    def search(self, canonicalBoard, dirichlet_noise=False):
+    def search(self, canonicalBoard):
         """
         This function performs one iteration of MCTS. It is recursively called
         till a leaf node is found. The action chosen at each node is one that
@@ -94,8 +89,6 @@ class MCTS():
 
             valids = self.game.getValidMoves(canonicalBoard, 1)
             self.Ps[s] = self.Ps[s] * valids  # masking invalid moves
-            if dirichlet_noise:
-                self.applyDirNoise(s, valids)
             sum_Ps_s = np.sum(self.Ps[s])
             if sum_Ps_s > 0:
                 self.Ps[s] /= sum_Ps_s  # renormalize
@@ -113,10 +106,6 @@ class MCTS():
             return -v
 
         valids = self.Vs[s]
-        if dirichlet_noise:
-            self.applyDirNoise(s, valids)
-            sum_Ps_s = np.sum(self.Ps[s])
-            self.Ps[s] /= sum_Ps_s  # renormalize
         cur_best = -float('inf')
         best_act = -1
 
@@ -152,13 +141,3 @@ class MCTS():
 
         self.Ns[s] += 1
         return -v
-
-    def applyDirNoise(self, s, valids):
-        dir_values = np.random.dirichlet(
-            [self.args.dirichletAlpha] * np.count_nonzero(valids))
-        dir_idx = 0
-        for idx in range(len(self.Ps[s])):
-            if self.Ps[s][idx]:
-                self.Ps[s][idx] = (0.75 * self.Ps[s][idx]) + (
-                    0.25 * dir_values[dir_idx])
-                dir_idx += 1
